9ebc044d311f055f8401ef745eefd20d7b8aebe8
Erik Weibust <erik@weibust.net>
2013-07-08 23:10:28 +0000

    Changed web.js to read from file

--- web.js
+++ web.js
@@ -2,8 +2,14 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var buf = new Buffer();
+var fs = require('fs');
+var output = '';
+output = fs.readFileSync('index.html');
+
 app.get('/', function(request, response) {
   response.send('Hello World 2!');
+  response.send( output );
 });
 
 var port = process.env.PORT || 5000;
